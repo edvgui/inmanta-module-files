4007596ba5190f04415dd7534a1daf30894fec50
Guillaume Everarts de Velp <edvgui@gmail.com>
2024-02-13 20:04:26 +0100

    Try to see file permissions before failure

--- inmanta_plugins/files/base.py
+++ inmanta_plugins/files/base.py
@@ -1,19 +1,19 @@
 """
-    Copyright 2023 Guillaume Everarts de Velp
+Copyright 2023 Guillaume Everarts de Velp
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-    Contact: edvgui@gmail.com
+Contact: edvgui@gmail.com
 """
 
 import typing
@@ -54,6 +54,11 @@ class BaseFileHandler(inmanta.agent.handler.CRUDHandlerGeneric[X]):
         self, ctx: inmanta.agent.handler.HandlerContext, resource: X
     ) -> None:
         self._io.chmod(resource.path, str(resource.permissions))
+
+        import logging
+
+        logging.error(self._io.file_stat(resource.path))
+
         self._io.chown(resource.path, resource.owner, resource.group)
         ctx.set_created()
 
